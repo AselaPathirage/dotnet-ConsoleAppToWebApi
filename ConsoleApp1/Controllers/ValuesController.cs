4a791cbd399403d844c19c0ebc44e95623f12930
AselaPathirage <aselapathirage@gmail.com>
2022-05-02 15:45:36 +0530

    Routes added

--- ConsoleApp1/Controllers/ValuesController.cs
+++ ConsoleApp1/Controllers/ValuesController.cs
@@ -3,16 +3,23 @@
 namespace ConsoleApptoAPI.Controllers
 {
 	[Route("api/[controller]")]
+	[Route("api/[controller]/[action]")]
 	[ApiController]
 	public class ValuesController : ControllerBase
 	{
 		[Route("/get-all")]
+		[Route("/getall")]
+		[Route("getall")] //api/values/getall - this works even this line doesnt exits
+		[Route("/[controller]/[action]")]
 		public string GetAll()
 		{
 			return "Hello getall";
 		}
 
-		[Route("/get-all-authors")]
+		//[Route("/get-all-authors")]
+		//[Route("/[action]/[controller]")]
+
+		// api/values/getallauthors - works bcuz of no routes defined specifically to this method
 		public string GetAllAuthors()
 		{
 			return "get all auhtors";
@@ -25,7 +32,7 @@ namespace ConsoleApptoAPI.Controllers
 		}
 
 		// Query String in Routing
-		[Route("search")]
+		[Route("/search")]
 		public string GetAllBooks(int id, int authorId, string name, int rating, int price)
 		{
 			return "get from search";
