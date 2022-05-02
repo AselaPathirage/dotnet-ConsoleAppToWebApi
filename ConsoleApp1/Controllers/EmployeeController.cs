203489ea80487f8d77ea59e55df9c6e5c3b751e1
AselaPathirage <aselapathirage@gmail.com>
2022-05-03 03:08:48 +0530

    Return types

---
+++ ConsoleApp1/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApptoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApptoAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmployeeController : ControllerBase
+	{
+		[Route("")]
+		public EmployeeModel GetEmployees()
+		{
+			return new EmployeeModel() { Id = 1, Name = "emp1" };
+		}
+
+		[Route("{id}")]
+		public IActionResult GetEmployeesById(int id)
+		{
+			if (id == 0)
+			{
+				return NotFound();
+			}
+			return Ok(new List<EmployeeModel>(){
+				new EmployeeModel() { Id = 1, Name = "emp1" },
+				new EmployeeModel() { Id = 2, Name = "emp2" }
+			});
+		}
+
+		[Route("{id}/basic")]
+		public ActionResult<EmployeeModel> GetEmployeesByIdBasicInformation(int id)
+		{
+			if (id == 0)
+			{
+				return NotFound();
+			}
+			return new EmployeeModel() { Id = 1, Name = "emp1" };
+		}
+	}
+}
