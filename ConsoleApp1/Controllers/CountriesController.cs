b96f7f21b54d3ba6cb1c65d10cf7c0b32f620592
AselaPathirage <aselapathirage@gmail.com>
2022-05-03 21:54:39 +0530

    BindProperty & BindProperties

---
+++ ConsoleApp1/Controllers/CountriesController.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApptoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApptoAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	//[BindProperties]
+	public class CountriesController : ControllerBase
+	{
+		//	[BindProperty]
+		//	public string Name { get; set; }
+		//	[BindProperty]
+		//	public int Population { get; set; }
+		//	[BindProperty]
+		//	public int Area { get; set; }
+
+		//[BindProperty]
+		[BindProperty(SupportsGet =true)] //httpget
+		public CountryModel Country { get; set; }
+
+		[HttpPost("")]
+		public IActionResult AddCountry()
+		{
+			return Ok($"Name={this.Country.Name},Population={this.Country.Population},Area={this.Country.Area}");
+		}
+	}
+}
---
+++ ConsoleApp1/Models/CountryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApptoAPI.Models
+{
+	public class CountryModel
+	{
+		public string Name { get; set; }
+
+		public int Population { get; set; }
+
+		
+		public int Area { get; set; }
+	}
+}
