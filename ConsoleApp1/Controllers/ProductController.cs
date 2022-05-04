f0ce38b3f042ee76ca0c75d31bc9f27714557f32
AselaPathirage <aselapathirage@gmail.com>
2022-05-05 00:13:39 +0530

    without singleton sservice

---
+++ ConsoleApp1/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApptoAPI.Models;
+using ConsoleApptoAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApptoAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly ProductRepository productRepository;
+
+		public ProductController()
+		{
+			productRepository = new ProductRepository();
+		}
+
+		[HttpPost("")]
+		public IActionResult AddProduct([FromBody] ProductModel product)
+		{
+			productRepository.AddProduct(product);
+			var products = productRepository.GetAllProducts();
+			return Ok(products);
+		}
+	}
+}
